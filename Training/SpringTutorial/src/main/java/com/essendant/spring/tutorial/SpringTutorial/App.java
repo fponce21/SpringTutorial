a13941bbc2dd782cf0244b3df55e2a6d66011f1c
fponce21 <94322184+fponce21@users.noreply.github.com>
2022-09-02 13:35:51 -0700

    Add files via upload

---
+++ Training/SpringTutorial/src/main/java/com/essendant/spring/tutorial/SpringTutorial/App.java
@@ -0,0 +1,20 @@
+package com.essendant.spring.tutorial.SpringTutorial;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args ){
+       ApplicationContext context = new ClassPathXmlApplicationContext("com/essendant/spring/tutorial/SpringTutorial/beans/beans.xml");
+    	Person person = (Person)context.getBean("person");
+    	person.speak();
+    	
+    	Address address = (Address)context.getBean("address");
+    	System.out.println(address);
+    	
+    	System.out.println(person);
+    	
+    	((ClassPathXmlApplicationContext)context).close();
+    }
+}
